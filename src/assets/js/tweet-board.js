0e803e87c288c803fdca1573d7c3aab123ba6c35
Aymeric Poude <aymeric.poude@gmail.com>
2018-05-04 21:44:47 +0200

    added ajax call to refresh total likes

--- src/assets/js/tweet-board.js
+++ src/assets/js/tweet-board.js
@@ -7,14 +7,32 @@ window.jQuery = $;
 $(document).ready(
     function () {
         $('.js-like').click(function() {
-            var messageId = $(this).data('id');
+            var el = $(this);
+            var messageId = el.data('id');
             $.ajax({
                 url: "/api/tweet_likes",
                 type: "POST",
                 dataType: "json",
                 contentType: "application/json; charset=utf-8",
-                data: '{"Message": "/api/tweet_messages/' + messageId + '"}'
-            });
+                data: '{"Message": "/api/tweet_messages/' + messageId + '"}',
+                success: function(result) {
+                    getLikesCount(el, messageId);
+                }
+            })
         });
     }
 );
+
+
+getLikesCount = function(el, messageId) {
+    $.ajax({
+        url: "/api/tweet_messages/" + messageId,
+        type: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function(result) {
+            var totalLikes = result.likes.length;
+            $(el).closest('.js-TweetMessage-Likes').find('.js-likesCount').html(totalLikes);
+        }
+    });
+}
