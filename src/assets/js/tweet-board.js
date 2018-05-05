6812716357be7c384ab5aaf516cd3dade5eeeb7b
Aymeric Poude <aymeric.poude@gmail.com>
2018-05-05 14:17:54 +0200

    reduced likes refresh & added show/hide on it

--- src/assets/js/tweet-board.js
+++ src/assets/js/tweet-board.js
@@ -4,35 +4,41 @@ const $ = require('jquery');
 window.$ = $;
 window.jQuery = $;
 
+const API_TWEET_LIKES = '/api/tweet_likes';
+const API_TWEET_MESSAGES = '/api/tweet_messages/';
+const CLASS_JS_LIKES_COUNT = '.js-likesMessageId-';
+
 $(document).ready(
     function () {
         $('.js-like').click(function() {
-            var el = $(this);
-            var messageId = el.data('id');
+            var messageId = $(this).data('id');
+            $(CLASS_JS_LIKES_COUNT + messageId).css('visibility', 'hidden');
             $.ajax({
-                url: "/api/tweet_likes",
+                url: API_TWEET_LIKES,
                 type: "POST",
                 dataType: "json",
                 contentType: "application/json; charset=utf-8",
                 data: '{"Message": "/api/tweet_messages/' + messageId + '"}',
-                success: function(result) {
-                    getLikesCount(el, messageId);
-                }
+                success: function() {
+                    updateLikesCount(messageId);
+
+                },
             })
         });
     }
 );
 
 
-getLikesCount = function(el, messageId) {
+updateLikesCount = function(messageId) {
     $.ajax({
-        url: "/api/tweet_messages/" + messageId,
+        url: API_TWEET_MESSAGES + messageId,
         type: "GET",
         dataType: "json",
         contentType: "application/json; charset=utf-8",
         success: function(result) {
-            var totalLikes = result.likes.length;
-            $(el).closest('.js-TweetMessage-Likes').find('.js-likesCount').html(totalLikes);
+            var total = result.likes.length;
+            $(CLASS_JS_LIKES_COUNT + messageId).html(total);
+            $(CLASS_JS_LIKES_COUNT + messageId).css('visibility', 'visible');
         }
     });
 }
