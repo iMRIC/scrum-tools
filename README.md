58a84d51b695c953b02bdc8881693a365a82b22b
iMRIC <aymeric.poude@gmail.com>
2018-06-01 12:10:47 +0200

    Added makefile to build/start and added doctrine migration script

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Scrum Toolbox
 
 [![Build Status](https://travis-ci.org/iMRIC/scrum-tools.svg?branch=master)](https://travis-ci.org/iMRIC/scrum-tools)
 [![Build Status](https://scrutinizer-ci.com/g/iMRIC/scrum-tools/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/iMRIC/scrum-tools)
+
+## Install
+
+    make          # self documented makefile
+    make install  # install and start the project
---
+++ src/Migrations/Version20180601093821.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180601093821 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE sprint (id INT AUTO_INCREMENT NOT NULL, number INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tweet_like (id INT AUTO_INCREMENT NOT NULL, message_id INT NOT NULL, INDEX IDX_6EB8A677537A1329 (message_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tweet_message (id INT AUTO_INCREMENT NOT NULL, sprint_id INT NOT NULL, message VARCHAR(255) NOT NULL, INDEX IDX_393E51178C24077B (sprint_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE fos_user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', UNIQUE INDEX UNIQ_957A647992FC23A8 (username_canonical), UNIQUE INDEX UNIQ_957A6479A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_957A6479C05FB297 (confirmation_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE tweet_like ADD CONSTRAINT FK_6EB8A677537A1329 FOREIGN KEY (message_id) REFERENCES tweet_message (id)');
+        $this->addSql('ALTER TABLE tweet_message ADD CONSTRAINT FK_393E51178C24077B FOREIGN KEY (sprint_id) REFERENCES sprint (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE tweet_message DROP FOREIGN KEY FK_393E51178C24077B');
+        $this->addSql('ALTER TABLE tweet_like DROP FOREIGN KEY FK_6EB8A677537A1329');
+        $this->addSql('DROP TABLE sprint');
+        $this->addSql('DROP TABLE tweet_like');
+        $this->addSql('DROP TABLE tweet_message');
+        $this->addSql('DROP TABLE fos_user');
+    }
+}
